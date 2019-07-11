157e5726e67c96343a93733d2fd356795ecbce24
Ronnie McGregor <ronjon31@gmail.com>
2019-07-11 11:13:09 -0700

    make grid generation based off list of clips

--- src/components/rolf.js
+++ src/components/rolf.js
@@ -2,39 +2,46 @@ import React, { useState } from "react";
 import { Grid, Image, Header, Icon, Container } from "semantic-ui-react";
 
 const Rolf = () => {
-  const rolfs = {
-    bees: ["bees1", "bees2"],
-    yeah_buddy: [
-      "yeah_buddy1",
-      "yeah_buddy2",
-      "yeah_buddy3",
-      "yeah_buddy4",
-      "yeah_buddy5"
-    ],
-    question: ["question1"],
-    working_home: ["working_home1", "working_home2"]
-  };
-
-  const rolfs2 = [
+  const rolfs = [
     {
       label: "Yeah buddy 1",
-      clip: "yeah_buddy_1"
+      clip: "yeah_buddy1"
     },
     {
       label: "Yeah buddy 2",
-      clip: "yeah_buddy_2"
+      clip: "yeah_buddy2"
     },
     {
       label: "Yeah buddy 3",
-      clip: "yeah_buddy_3"
+      clip: "yeah_buddy3"
     },
     {
       label: "Yeah buddy 4",
-      clip: "yeah_buddy_4"
+      clip: "yeah_buddy4"
     },
     {
       label: "Yeah buddy 5",
-      clip: "yeah_buddy_5"
+      clip: "yeah_buddy5"
+    },
+    {
+      label: "Bees 1",
+      clip: "bees1"
+    },
+    {
+      label: "Bees 2",
+      clip: "bees2"
+    },
+    {
+      label: "Question 1",
+      clip: "question1"
+    },
+    {
+      label: "Working from home 1",
+      clip: "working_home1"
+    },
+    {
+      label: "Working from home 2",
+      clip: "working_home2"
     }
   ];
 
@@ -43,11 +50,10 @@ const Rolf = () => {
     setSound(false);
   });
   const playClip = clip => {
-    const clips = rolfs[clip];
-    const random = clips[Math.floor(Math.random() * clips.length)];
+    console.log(clip);
     audio.pause();
     audio.currentTime = 0;
-    audio.src = "/assets/" + random + ".mp3";
+    audio.src = "/assets/" + clip + ".mp3";
     audio.play();
     setSound(true);
   };
@@ -55,73 +61,38 @@ const Rolf = () => {
   const style = {
     width: "100px",
     height: "100px",
-    cursor: "pointer"
+    cursor: "pointer",
+    marginTop: "20px",
+    marginBottom: "90px",
+    marginLeft: "20px",
+    marginRight: "20px"
   };
 
   const [sound, setSound] = useState(false);
   return (
     <Container>
-      <Grid centered columns={2}>
-        <Grid.Column>
-          <Header as="h3" textAlign="center">
-            Click The Rolf &nbsp;
-            <Icon name={sound ? "volume up" : "volume off"} centered />
-          </Header>
-        </Grid.Column>
-
-        <Grid.Row centered columns={2}>
-          <Grid.Column>
-            <Image
-              style={style}
-              onClick={() => playClip("yeah_buddy")}
-              src="/assets/jay_face.png"
-              size="mini"
-              centered
-            />
-            <Header as="h3" textAlign="center">
-              Yeah Buddy
-            </Header>
-          </Grid.Column>
-          <Grid.Column>
-            <Image
-              style={style}
-              onClick={() => playClip("bees")}
-              src="/assets/jay_face.png"
-              size="mini"
-              centered
-            />
-            <Header as="h3" textAlign="center">
-              Bees
-            </Header>
-          </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row centered columns={2}>
-          <Grid.Column>
-            <Image
-              style={style}
-              onClick={() => playClip("question")}
-              src="/assets/jay_face.png"
-              size="mini"
-              centered
-            />
-            <Header as="h3" textAlign="center">
-              Question for you
-            </Header>
-          </Grid.Column>
-          <Grid.Column>
-            <Image
-              style={style}
-              onClick={() => playClip("working_home")}
-              src="/assets/jay_face.png"
-              size="mini"
-              centered
-            />
-            <Header as="h3" textAlign="center">
-              Working from home
-            </Header>
-          </Grid.Column>
-        </Grid.Row>
+      <Grid.Column>
+        <Header as="h3" textAlign="center">
+          Click The Rolf &nbsp;
+          <Icon name={sound ? "volume up" : "volume off"} />
+        </Header>
+      </Grid.Column>
+      <Grid>
+        {rolfs.map((quote, index) => {
+          return (
+            <Grid.Column key={index} style={style}>
+              <Image
+                onClick={() => playClip(quote.clip)}
+                src="/assets/jay_face.png"
+                size="small"
+                centered
+              />
+              <Header as="h3" textAlign="center">
+                {quote.label}
+              </Header>
+            </Grid.Column>
+          );
+        })}
       </Grid>
     </Container>
   );
